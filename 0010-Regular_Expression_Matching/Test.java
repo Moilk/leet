337f962ebc0ab3aedbdb6ecd585f82d49be12076
moilk <moilk@qq.com>
2019-05-22 00:13:08 +0800

    backup 0010

--- 0010-Regular_Expression_Matching/Test.java
+++ 0010-Regular_Expression_Matching/Test.java
@@ -1,13 +1,45 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        
-        return false;
+    	System.out.println(s + " | " + p);
+    	if(p.isEmpty()){
+    		return s.isEmpty();
+    	}
+
+    	boolean firstMatch = (!s.isEmpty())&&((p.charAt(0) == '.')|| (p.charAt(0) == s.charAt(0)));
+
+    	if(p.length()>=2){
+    		if(p.charAt(1) == '*'){
+    			if(s.isEmpty())	return true;
+
+    			return s.isEmpty() || (firstMatch && isMatch(s.substring(1), p));
+    		}
+    		return firstMatch && isMatch(s.substring(1), p.substring(1));
+    	}
+    	return firstMatch && (s.length()<2);
     }
 }
 
 public class Test{
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		
+		String[] ss = {
+			"aa",
+			"aa",
+			"ab",
+			"aab",
+			"mississippi",
+
+		};
+		String[] pp = {
+			"a",
+			"a*",
+			".*",
+			"c*a*b",
+			"mis*is*p*.",
+
+		};
+
+		for(int i=0; i<ss.length; i++){
+			System.out.println(ss[i]+" | "+ pp[i] +" => "+new Solution().isMatch(ss[i], pp[i]));
+		}
 	}
 }
