2bffce68e4a225d1cf5086fb238ef73f68745677
moilk <moilk@qq.com>
2019-05-29 00:08:01 +0800

    backup

---
+++ 0023-Merge_k_Sorted_Lists/Solution.java
@@ -0,0 +1,14 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+    	ListNode res = new ListNode(0);
+        
+
+        return res.next;
+    }
+}
