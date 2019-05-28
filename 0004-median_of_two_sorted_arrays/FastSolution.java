a2083d8ec2c862db416137305f71fc2e86273a59
moilk <moilk@qq.com>
2019-05-28 12:03:51 +0800

    backup 4

--- 0004-median_of_two_sorted_arrays/FastSolution.java
+++ 0004-median_of_two_sorted_arrays/FastSolution.java
@@ -1,7 +1,29 @@
 public class FastSolution {
 
-	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		
+	public double findMedianSortedArrays(int[] A, int[] B) {
+		if(A.length > B.length) {
+			int[] tmp = A;
+			A = B;
+			B = tmp;
+		}
+
+		int m = A.length;
+		int n = B.length;
+
+		int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+		while (iMin <= iMax) {
+			int i =  iMin + (iMax - iMin) / 2;
+			int j = halfLen - i;
+
+			if (B[j - 1] > A[i]) {
+				iMin = i + 1;
+			} else if (A[i - 1] > B[j]) {
+				iMax = i - 1;
+			} else {
+				
+			}
+
+		}
 
 		return 0.0;
 	}
