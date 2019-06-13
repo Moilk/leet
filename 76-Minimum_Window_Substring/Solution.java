5e17f50b1242d744b8a8a9d4fa50c6a117f3d3f5
Moilk <moilk@qq.com>
2019-06-13 10:59:04 +0800

    accept 76

--- 76-Minimum_Window_Substring/Solution.java
+++ 76-Minimum_Window_Substring/Solution.java
@@ -1,69 +1,50 @@
 import java.util.*;
 
 class Solution {
+	int[] asciiCount = new int[256];
 
-    public String minWindow(String s, String t) {
-    	Map<Character, Integer> charCount = new HashMap<>();
-
-    	for(int i = 0; i < t.length(); i++) {
-    		char c = t.charAt(i);
-    		if(!charCount.containsKey(c)){
-    			charCount.put(c, 1);
-    		}else{
-    			charCount.put(c, charCount.get(c) + 1);
-    		}
-    	}
-
-    	int index = 0;
-    	int minLen = Integer.MAX_VALUE;
-    	int count = 0;
-    	int l = 0, r = 0;
-
-    	while(r < s.length()) {
-    		char c = s.charAt(r);
-    		if(charCount.containsKey(c)){
-    			int  tmp = charCount.get(c);
-    			if(tmp > 0) {
-    				charCount.put(c, tmp - 1);
-    				count++;
-    				r++;
-    				if(count == t.length()) {
-    					while(!charCount.containsKey(s.charAt(l)))	l++;
-    					int len = r - l;
-    					if(len < minLen) {
-    						minLen = len;
-    						index = l;
-    						if(minLen == t.length()){
-    							break;
-    						}
-    					}
-    				}
-    			} else {
-    				c = s.charAt(l);
-    				if(charCount.containsKey(c)){
-    					charCount.put(c, charCount.get(c) + 1);
-    					count--;
-    				}
-    				l++;
-    			}
-
-    		} else {
-    			r++;
-    		}
-
+	public String minWindow(String s, String t) {
+		for (char c : t.toCharArray()) {
+			asciiCount[c]++;
 		}
-		
-		if(minLen == Integer.MAX_VALUE)	return "";
 
-    	return s.substring(index, index + minLen);	
-    }
+		int index = 0;
+		int minLen = Integer.MAX_VALUE;
+		char[] S = s.toCharArray();
+		int l = 0, r = 0;
+		int left = t.length();
 
-    public static void main(String[] args) {
-    	String S = "a";
-    	String T = "aa";
+		while (r < S.length) {
+			if (asciiCount[S[r]] > 0) {
+				left--;
+			}
+			asciiCount[S[r]]--;
+			r++;
 
-    	Solution s = new Solution();
+			if (left == 0) {
+				while (asciiCount[S[l]] < 0) {
+					asciiCount[S[l]]++;
+					l++;
+				}
+				int len = r - l;
+				if (len < minLen) {
+					minLen = len;
+					index = l;
+				}
+			}
+		}
 
-    	System.out.println(s.minWindow(S, T));
-    }
+		if (minLen == Integer.MAX_VALUE)
+			return "";
+		return s.substring(index, index + minLen);
+	}
+
+	public static void main(String[] args) {
+		String S = "ADOBECODEBANC";
+		String T = "ABC";
+
+		Solution s = new Solution();
+
+		System.out.println(s.minWindow(S, T));
+	}
 }
