79340a6a07c6b7a60c0cd187743f7e80b5efbf76
moilk <moilk@qq.com>
2019-06-24 22:21:42 +0800

    backup

---
+++ 143-Reorder_List/Solution.java
@@ -0,0 +1,13 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    private ListNode left = head;
+
+    public void reorderList(ListNode head) {
+        if(head.next) 
+    }
+}
