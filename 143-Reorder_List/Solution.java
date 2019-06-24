04f682bad389ac98cb020283dd5153ceea1ef6a9
moilk <moilk@qq.com>
2019-06-24 23:39:52 +0800

    AC 143 重排链表

--- 143-Reorder_List/Solution.java
+++ 143-Reorder_List/Solution.java
@@ -1,13 +1,35 @@
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
-
 class Solution {
-    private ListNode left = head;
+    private ListNode left;
+    private boolean stop;
 
     public void reorderList(ListNode head) {
-        if(head.next) 
+        left = head;
+        stop = false;
+        helper(head);
+    }
+
+    private void helper(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+
+        helper(head.next);
+
+        if(stop) return;
+        if (head == left || head == left.next) {
+            stop = true;
+        }
+        ListNode cur = head.next;
+        head.next = head.next.next;
+        cur.next = left.next;
+        left.next = cur;
+        left = cur.next;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 4};
+        ListNode head = ListNode.fromArray(a);
+        Solution s = new Solution();
+        s.reorderList(head);
+        System.out.println(head);
     }
 }
